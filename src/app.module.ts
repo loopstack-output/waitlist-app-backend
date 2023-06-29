30dd3fa6b479780bad5c68f3bd08dc0141fb3fd8
Jakob <jakob.klippel@config.one>
2023-06-29 16:37:56 +0200

    Add event emitter configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeormConfig from './typeorm.config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
       ConfigModule.forRoot({
           isGlobal: true
       }),
-      TypeOrmModule.forRoot(typeormConfig)
+      TypeOrmModule.forRoot(typeormConfig),
+      EventEmitterModule.forRoot()
   ],
   controllers: [],
   providers: [],
